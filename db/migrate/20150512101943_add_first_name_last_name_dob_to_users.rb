2d696a6ae78edc52402e979edf369df75c62aa9e
Vinh Hoang <hoangzinhvn@gmail.com>
2015-05-12 17:57:39 +0700

    add firstname, lastname, dob field into User table

---
+++ db/migrate/20150512101943_add_first_name_last_name_dob_to_users.rb
@@ -0,0 +1,7 @@
+class AddFirstNameLastNameDobToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :firstname, :string
+    add_column :users, :lastname, :string
+    add_column :users, :dob, :datetime
+  end
+end
