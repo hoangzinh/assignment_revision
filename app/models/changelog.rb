39538557bba2be669e0c84c9ad6724d33bc82e0f
Vinh Hoang <hoangzinhvn@gmail.com>
2015-05-15 14:26:30 +0700

    add validation for changelog and history model

--- app/models/changelog.rb
+++ app/models/changelog.rb
@@ -1,3 +1,7 @@
 class Changelog < ActiveRecord::Base
+  validates :data_name, presence: true
+  validates :data_from, presence: true
+  validates :data_to, presence: true
+
   belongs_to :history
 end
--- app/models/history.rb
+++ app/models/history.rb
@@ -1,4 +1,6 @@
 class History < ActiveRecord::Base
+  validates :editer_id, presence: true
+  validates :edited_id, presence: true
   belongs_to :editer, class_name: "User"
   belongs_to :edited, class_name: "User"
   has_many :changelogs, dependent: :destroy
