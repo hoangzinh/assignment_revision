4ad934ffeaaa43d341d763d3c1d6b24b5647cdc0
Vinh Hoang <hoangzinhvn@gmail.com>
2015-05-16 16:36:58 +0700

    update foreign key of :editing_histories

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :edited_histories, class_name: "History", foreign_key: "edited_id", dependent: :destroy
-  has_many :editing_histories, class_name: "History", foreign_key: "editing_id", dependent: :destroy
+  has_many :editing_histories, class_name: "History", foreign_key: "editer_id", dependent: :destroy
   has_many :editeds, through: :edited_histories
   has_many :editings, through: :editing_histories
 end
